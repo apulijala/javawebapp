ace1be362ff633a72a7a3cbdab17b0e1d80b065e
root <root@ip-172-31-34-33.eu-west-2.compute.internal>
2023-03-04 19:58:28 +0000

    added code deploy files here

---
+++ scripts/stoptomcat.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+ 
+/opt/apache-tomcat-9.0.73/bin/shutdown.sh
+# Copying the files.
+if [ -f "/opt/apache-tomcat-9.0.73/webapps/" ]
+then
+  rm -rf /opt/apache-tomcat-9.0.73/webapps/javawebapp*
+fi
+cp -f  /tmp/javawebapp.war /opt/apache-tomcat-9.0.73/webapps/
+/opt/apache-tomcat-9.0.73/bin/startup.sh
+~                                           
