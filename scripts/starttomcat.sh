00f8ff146603b766c1f46c949e735abef0bc77c7
apulijala <arvind.pulijala@yahoo.com>
2023-03-04 20:33:47 +0000

    Update starttomcat.sh

--- scripts/starttomcat.sh
+++ scripts/starttomcat.sh
@@ -7,8 +7,6 @@ then
   rm -rf /opt/apache-tomcat-9.0.73/webapps/javawebapp*
 fi
  
-cp -f  /tmp/javawebapp.war /opt/apache-tomcat-9.0.73/webapps/
+cp -f  /tmp/target/javawebapp.war /opt/apache-tomcat-9.0.73/webapps/
  
 /opt/apache-tomcat-9.0.73/bin/startup.sh
-~                                                                        
-~                                             
