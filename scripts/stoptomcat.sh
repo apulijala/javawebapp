0bc0eedf5b137c6f2999b089a6b28148d413dce6
root <root@ip-172-31-34-33.eu-west-2.compute.internal>
2023-03-04 20:25:04 +0000

    fix: added path correctly

--- scripts/stoptomcat.sh
+++ scripts/stoptomcat.sh
@@ -6,6 +6,6 @@ if [ -f "/opt/apache-tomcat-9.0.73/webapps/" ]
 then
   rm -rf /opt/apache-tomcat-9.0.73/webapps/javawebapp*
 fi
-cp -f  /tmp/javawebapp.war /opt/apache-tomcat-9.0.73/webapps/
+cp -f  /tmp/target/javawebapp.war  /opt/apache-tomcat-9.0.73/webapps/
 /opt/apache-tomcat-9.0.73/bin/startup.sh
 ~                                           
